2f67efdbf7a0b8f1fc63b05b5bd69561a42f4547
Maximiliano Mendez <227851002+MaisiORG@users.noreply.github.com>
2026-05-26 17:14:58 -0300

    Enhance Main.java with decimal stack and pair example
    Added additional output for decimal stack and pair exercise.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
-public class Main {
-    public static void main(String[] args) {
 
-        System.out.println("PILA DE NUMEROS");
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("XXXXX EJERCICIO 1 PILAS XXXXX");
+        System.out.println("");
 
         Pila<Integer> pilaNumeros = new Pila<>();
 
@@ -16,7 +18,9 @@ public class Main {
         System.out.println("Numero está vacio?: "+pilaNumeros.estaVacia());
         System.out.println("Tamaño del la Pila: "+pilaNumeros.tamanio());
 
+        System.out.println("");
         System.out.println("PILA DE TEXTOS");
+        System.out.println("");
 
         Pila<String> pilaTextos = new Pila<>();
 
@@ -26,7 +30,9 @@ public class Main {
         System.out.println("Texto que se va a desapilar:" +pilaTextos.desapilar());
         System.out.println("Texto esta vacio? "+pilaTextos.estaVacia());
 
+        System.out.println("");
         System.out.println("PILA DE NUMEROS DECIMALES");
+        System.out.println("");
 
         Pila<Double> pilaDecimales = new Pila<>();
 
@@ -40,5 +46,21 @@ public class Main {
         System.out.println("Decimal en la cima: "+pilaDecimales.cima());
         System.out.println("Decimal está vacio?: "+pilaDecimales.estaVacia());
         System.out.println("Tamaño del la Pila: "+pilaDecimales.tamanio());
+
+
+        System.out.println("");
+        System.out.println("---- EJERCICIO 3: PAR<A,B> ----");
+        System.out.println("");
+
+        // Se crea el par original con dos valores
+        Par<Integer, String> parOriginal = new Par<>(1234, "Milanesas");
+        System.out.println("Par original: " + parOriginal);
+
+        // Usamos el método invertir() para obtener el par invertido
+        // Se cambia el orden de los tipos de datos de <Integer, String> a <String, Integer>
+        Par<String, Integer> parInvertido = parOriginal.invertir();
+        System.out.println("Par invertido: " + parInvertido);
+
+
     }
 }
